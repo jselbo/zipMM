3ead99ee2e04956afc1367bde57e9bc07b17ef21
cs240 <mgottein@purdue.edu>
2013-04-09 16:08:00 -0400

    made hash table actually work! properly does quadratic probing now and uses load factor to determine when to resize. thanks to jason for the help

--- hash_table.c
+++ hash_table.c
@@ -1,7 +1,9 @@
 #include "hash_table.h"
 #include <stdlib.h>
-#include <stdio.h>
+
 #define DEFAULT_SIZE 512
+#define LOAD_FACTOR_THRESHOLD .75
+#define KEY_INC(K, I, M) K = ((K % M) + ((I % M) * (I % M)) % M) % M
 
 struct pair {
 	void* k;
@@ -9,15 +11,16 @@ struct pair {
 };
 
 struct _hash_table {
-	int(*hash)(void*);
+	unsigned int(*hash)(void*);
 	int(*cmp)(void*,void*);
 	struct pair* slot;
 	int size;
 	int free_k;
 	int free_v;
+	int occupied;
 };
 
-hash_table ht_alloc(int(*hash)(void*), int(*cmp)(void*,void*), int free_k, int free_v) {
+hash_table ht_alloc(unsigned int(*hash)(void*), int(*cmp)(void*,void*), int free_k, int free_v) {
 	hash_table tbl = (hash_table)malloc(sizeof(struct _hash_table));
 	tbl->hash = hash;
 	tbl->cmp = cmp;
@@ -25,53 +28,65 @@ hash_table ht_alloc(int(*hash)(void*), int(*cmp)(void*,void*), int free_k, int f
 	tbl->free_k = free_k;
 	tbl->free_v = free_v;
 	tbl->size = DEFAULT_SIZE;
+	tbl->occupied = 0;
 }
 
 void* ht_lookup(hash_table tbl, void* k) {
-	int i, key = tbl->hash(k);
-	for(i = 0, key = (key + (i * i)) % tbl->size; i < tbl->size; ++i) {
+	unsigned int i, key = tbl->hash(k) % tbl->size;
+	for(i = 0; i < tbl->size; ++i) {
+		KEY_INC(key, i, tbl->size);
 		if(tbl->slot[key].k != NULL) {
 			if(tbl->cmp(tbl->slot[key].k, k) == 0) {
 				return tbl->slot[key].v;
 			}
 		}
+		//remove this clause if we can remove from hash tables
 		else {
 			return NULL;
 		}
 	}
+	return NULL;
 }
 
 void ht_insert(hash_table tbl, void* k, void* v) {
-	int i, key = tbl->hash(k), old_size;
+	unsigned int i, old_size, key = tbl->hash(k) % tbl->size;
 	struct pair* old_slot;
-	for(i = 0, key = (key + i * i) % tbl->size; i < tbl->size; ++i) {
+	for(i = 0; i < tbl->size; ++i) {
+		KEY_INC(key, i, tbl->size);
 		if(tbl->slot[key].k == NULL) {
 			tbl->slot[key].k = k;
 			tbl->slot[key].v = v;
-			return;
+			break;
 		}
 		else if(tbl->cmp(tbl->slot[key].k, k) == 0) {
 			tbl->slot[key].v = v;
-			return;
+			break;
 		}
+		
 	}
-	old_slot = tbl->slot;
-	old_size = tbl->size;
-	tbl->slot = (struct pair*)calloc(tbl->size * 2, sizeof(struct pair));
-	tbl->size *= 2;
-	//rehash the table
-	for(i = 0; i < old_size; ++i) {
-		if(old_slot[i].k != NULL) {
-			ht_insert(tbl, old_slot[i].k, old_slot[i].v);
+	tbl->occupied++;
+	if((double)tbl->occupied / (double)tbl->size > LOAD_FACTOR_THRESHOLD) {
+		old_slot = tbl->slot;
+		old_size = tbl->size;
+		tbl->slot = (struct pair*)calloc(tbl->size * 2, sizeof(struct pair));
+		tbl->size *= 2;
+		//rehash the table
+		for(i = 0; i < old_size; ++i) {
+			if(old_slot[i].k != NULL) {
+				ht_insert(tbl, old_slot[i].k, old_slot[i].v);
+			}
 		}
+		ht_insert(tbl, k, v);
+		free(old_slot);	
 	}
-	ht_insert(tbl, k, v);
-	free(old_slot);
+	
 }
 
+/*
 void ht_remove(hash_table tbl, void* k) {
-	int i, key = tbl->hash(k);
-	for(i = 0, key = (key + i * i) % tbl->size; i < tbl->size; ++i) {
+	unsigned int i, key = tbl->hash(k) % tbl->size;
+	for(i = 0; i < tbl->size; ++i) {
+		KEY_INC(key, i, tbl->size);
 		if(tbl->slot[key].k != NULL) {
 			if(tbl->cmp(tbl->slot[key].k, k) == 0) {
 				if(tbl->free_k)
@@ -83,11 +98,9 @@ void ht_remove(hash_table tbl, void* k) {
 				break;
 			}
 		}
-		else {
-			break;
-		}
 	}
 }
+*/
 
 void ht_free(hash_table tbl) {
 	int i;
--- hash_table.h
+++ hash_table.h
@@ -5,10 +5,10 @@ struct _hash_table;
 
 typedef struct _hash_table * hash_table;
 
-hash_table ht_alloc(int(*hash)(void*), int(*cmp)(void*, void*), int free_k, int free_v);
+hash_table ht_alloc(unsigned int(*hash)(void*), int(*cmp)(void*, void*), int free_k, int free_v);
 void* ht_lookup(hash_table tbl, void* k);
 void ht_insert(hash_table tbl, void* k, void* v);
-void ht_remove(hash_table tbl, void* k);
+//void ht_remove(hash_table tbl, void* k);
 void ht_free(hash_table tbl);
 
 #endif
