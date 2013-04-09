440fd77231114ff804ba614c795876dbeb7a00d0
cs240 <mgottein@purdue.edu>
2013-04-08 22:45:23 -0400

    added hash table implementation - using open addressing with quadratic probing

--- hash_table.c
+++ hash_table.c
@@ -5,31 +5,59 @@
 
 struct pair {
 	void* k;
-	void* p;
+	void* v;
 };
 
 struct _hash_table {
 	int(*hash)(void*);
+	int(*cmp)(void*,void*);
 	struct pair* slot;
 	int size;
 };
 
-hash_table ht_alloc(int(*hash)(void*)) {
+hash_table ht_alloc(int(*hash)(void*), int(*cmp)(void*,void*)) {
 	hash_table tbl = (hash_table)malloc(sizeof(struct _hash_table));
 	tbl->hash = hash;
-	tbl->slot = (struct pair*)(malloc(DEFAULT_SIZE * sizeof(struct pair)));
+	tbl->cmp = cmp;
+	tbl->slot = (struct pair*)(calloc(DEFAULT_SIZE,sizeof(struct pair)));
 }
 
 void* ht_lookup(hash_table tbl, void* k) {
-	return NULL;
+	int i, key = tbl->hash(k);
+	for(i = 0, key = (key + (i * i)) % tbl->size; i < tbl->size; ++i) {
+		if(tbl->slot[key].k != NULL && tbl->cmp(tbl->slot[key].k, k) == 0) {
+			return tbl->slot[key].v;
+		}
+	}
 }
 
-void* ht_insert(hash_table tbl, void* k, void* v) {
-	return NULL;
+void ht_insert(hash_table tbl, void* k, void* v) {
+	int i, key = tbl->hash(k);
+	for(i = 0, key = (key + i * i) % tbl->size; i < tbl->size; ++i) {
+		if(tbl->slot[key].k == NULL) {
+			tbl->slot[key].k = k;
+			tbl->slot[key].v = v;
+			break;
+		}
+		else if(tbl->cmp(tbl->slot[key].k, k) == 0) {
+			tbl->slot[key].v = v;
+			break;
+		}
+	}
+	//increase table size and try again
 }
 
-void* ht_remove(hash_table tbl, void* k, void* v) {
-	return NULL;
+void ht_remove(hash_table tbl, void* k) {
+	int i, key = tbl->hash(k);
+	for(i = 0, key = (key + i * i) % tbl->size; i < tbl->size; ++i) {
+		if(tbl->slot[key].k != NULL && tbl->cmp(tbl->slot[key].k, k) == 0) {
+			free(tbl->slot[key].k);
+			free(tbl->slot[key].v);
+			tbl->slot[key].k = NULL;
+			tbl->slot[key].v = NULL;
+			break;
+		}
+	}
 }
 
 void ht_free(hash_table tbl) {
